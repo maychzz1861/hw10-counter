8c18a28b0501419d3aa8038492cf794ca0f6776b
maychzz1861 <136160575+maychzz1861@users.noreply.github.com>
2023-12-26 13:57:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>)
+
+function Counter() {
+  return (
+   <div className='counter'>
+      <button> - </button>
+      <h3>0</h3>
+      <button> + </button>
+      <button> C </button>
+      <button> x </button>
+   </div>
+  )
+}
+
+function SumInfo(props)  {
+    const stTitle = {
+        color : props.color,
+        fonSize : props.size==='big' ? '50px' : '40px'
+
+    }
+
+  return (
+    <div className='suminfo'>
+      <h1 style={ { color: props.color}}>Sum = 0</h1>
+      <button className='text-center'>Add counter</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+  <>
+    <SumInfo color="black" size="samll"/> 
+    <Counter />
+  </>
+  )
+}
